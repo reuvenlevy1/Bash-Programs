1c74a0d3a47c51e2819facd130eb2a799f7edd78
reuvenlevy1 <reuvenlevy1@aol.com>
2019-03-29 16:52:52 -0400

    Update count(Without Arrays).sh

--- Interview Programs/count/count(Without Arrays).sh
+++ Interview Programs/count/count(Without Arrays).sh
@@ -5,16 +5,14 @@
 #@Reuven
 
 cd /bin
-
 COUNT=0
-for x in {a..z}
-do
-  for i in *
-  do
-    if [ "$x" = "${i:0:1}" ]; then
+
+for x in {a..z}; do                           #Loop a through z
+  for i in *; do                              #Loop everything in /bin directory
+    if [ "${i:0:1}" = "$x" ]; then            #If the first letter of the file/folder in the /bin directory matches the letter of the first for loop
       let "COUNT++"
     fi
   done
   echo "$x ... $COUNT"
-  COUNT=0
+  COUNT=0                                 
 done
