066174924baccea75aec572e72aa3a0f4bc1ff25
reuvenlevy1 <reuvenlevy1@aol.com>
2019-03-31 21:38:14 -0400

    Add files via upload

---
+++ Interview Programs/insert-sort/README.md
@@ -0,0 +1,8 @@
+Takes numbers inserted in any order and sorts them in numerical order.
+
+This is done in the following way:
+  1. Takes user input and stores into SORT array.
+  2. Compares current value with next and swaps if necessary.
+
+![program-execution](images/insert-sort.sh.png)
+  
---
+++ Interview Programs/insert-sort/insert-sort.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Takes numbers inserted in any order and sorts them in numerical order.
+#This is done in the following way:
+#1. Takes user input and stores into SORT array.
+#2. Compares current value with next and swaps if necessary.
+#
+#@Reuven
+
+USER_INPUT=$@                                               #Set parameters to USER_INPUT
+length=$#							    					#Length of parameters
+declare -a SORT=(${USER_INPUT[@]})				    		#Add all parameters to SORT array
+for (( i = 1; i < $length; i++ )); do
+  x="${SORT[$i]}"						    				#x is the SORT[] values
+  y=`expr $i - 1`						    				#y is the SORT[] index values
+  while [[ "$y" -ge 0 ]] && [[ "${SORT[$y]}" -gt "$x" ]]; do	#Checking if values need to be swapped
+    SORT[$y+1]="${SORT[$y]}"					    		#Swapping values
+    y=`expr $y - 1`						    				#Moving SORT[] index value
+  done
+SORT[$y+1]="$x"						            			#Move to next SORT[] index value
+done
+echo ${SORT[@]}
