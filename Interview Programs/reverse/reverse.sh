9d3a02a9ee1d5489bd0174486a8ef070ced74bb7
reuvenlevy1 <reuvenlevy1@aol.com>
2019-03-31 20:32:12 -0400

    Update reverse.sh

--- Interview Programs/reverse/reverse.sh
+++ Interview Programs/reverse/reverse.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-cd /                                #Change directory to /
+
+#Stores the files stored in root directory and reverses the contents and displays them.
+#This is done in the following way:
+#1. main() function - Stores files from root directory into CONTENTS array.
+#2. reverse() function - Stores the contents from the CONTENTS array in decrementing order
+#into the REVERSE array and echos the results.
+#
+#@Reuven
+
+cd /
 
 reverse(){
-  local z=0                         #Variable z onlye works for inside the reverse function
-  while [ $SIZE -ge 0 ]             #Is the size of the CONTENTS array greater than or equal to 0
-  do
+  local z=0
+  while [ $SIZE -ge 0 ]; do          #Is the size of the CONTENTS array greater than or equal to 0
     REVERSE[$z]=${CONTENTS[$SIZE]}
     let "SIZE--"
     let "z++"
   done
-  echo "Reversed: ${REVERSE[@]}"              #Prints everything in REVERSE array
+  echo "Reversed: ${REVERSE[@]}"     #Prints everything in REVERSE array
 }
 
 main(){
-  for i in *                        #the i variable is everything in the current directory (wildcard)
-  do
-    CONTENTS[$x]=$i
+  for i in *; do
+    CONTENTS[$x]=$i					 #Stores all contents within root directory into CONTENTS array
     let "x++"
   done
-  echo "Origial: ${CONTENTS[@]}"
+  echo "Original: ${CONTENTS[@]}"    #Prints everything in CONTENTS array
   SIZE=${#CONTENTS[@]}
   reverse                            #calls reverse function
 }
