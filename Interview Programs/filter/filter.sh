032893432cdae09347698ddadb0fcfdf82855963
reuvenlevy1 <reuvenlevy1@aol.com>
2019-04-03 17:43:45 -0400

    Update filter.sh

--- Interview Programs/filter/filter.sh
+++ Interview Programs/filter/filter.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#Displays certain files dependent upon their size by filtering out those less than the average.
-#This is done in the following way:
-#1. main() function - Stores names and sizes of files into arrays NAME and SIZE.
-#2. average() function - Calculates average size by dividing total size by number of files.
-#3. filter() function - Displays files with sizes greater than average.
+# Displays certain files dependent upon their size by filtering out those less than the average.
+# This is done in the following way:
+# 1. main() function - Stores names and sizes of files into arrays NAME and SIZE.
+# 2. average() function - Calculates average size by dividing total size by number of files.
+# 3. filter() function - Displays files with sizes greater than average.
 #
-#Limitations:
-#1. Delimiter is 1 space & only works w/ sizes in hundreths place.
+# Limitations:
+# 1. Delimiter is 1 space & only works w/ sizes in hundreths place.
 #
-#@Reuven
+# @author Reuven
 
 filter(){
   TOTAL=0
   echo ""
   echo "FILTERED:"
   for (( k = 0; k < $COUNT; k++ )); do
-    if [[ ${SIZE[$k]} -gt $AVG ]]; then	#Size greater than average size
+    if [[ ${SIZE[$k]} -gt $AVG ]]; then	      #Size greater than average size
       echo "${NAME[$k]} (${SIZE[$k]})"
     fi
   done
@@ -30,14 +30,14 @@ average(){
 
 main(){
   TOTAL=0
-  COUNT=0                               #Adds # of files in directory
+  COUNT=0                                     #Adds # of files in directory
   echo "UNFILTERED:"
-  for i in *; do                        #i is everything in the directory
+  for i in *; do                              #i is everything in the directory
     let "COUNT += 1"
   done
   for (( j = 0; j < $COUNT; j++ )); do
-    NAME=(`ls -l | cut -d ' ' -f 9`)    #() makes an array; Stores name of file (-f 9)
-    SIZE=(`ls -l | cut -d ' ' -f 5`)    #Finds data (size) in 5th spot (-f 5) & erases the space (delimiter) & everything before it
+    NAME=(`ls -l | cut -d ' ' -f 9`)          #() makes an array; Stores name of file (-f 9)
+    SIZE=(`ls -l | cut -d ' ' -f 5`)          #Finds data (size) in 5th spot (-f 5) & erases the space (delimiter) & everything before it
     echo "${NAME[$j]} (${SIZE[$j]})"
     let "TOTAL += ${SIZE[$j]}"
   done
