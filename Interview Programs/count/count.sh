1d529af51dcc7eb47058eab62add913de75aa35b
reuvenlevy1 <reuvenlevy1@aol.com>
2019-04-03 17:39:54 -0400

    Update count.sh

--- Interview Programs/count/count.sh
+++ Interview Programs/count/count.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Counts and lists the number of occurrences for files stored in "/bin" directory by first letter.
-#This is done in the following way:
-#1. Stores letters a-z into an array called LETTERS.
-#2. Stores a count of first letter occurrences that match the current letter in LETTERS
-#into an array called NUMBERS.
+# Counts and lists the number of occurrences for files stored in "/bin" directory by first letter.
+# This is done in the following way:
+# 1. Stores letters a-z into an array called LETTERS.
+# 2. Stores a count of first letter occurrences that match the current letter in LETTERS
+# into an array called NUMBERS.
 #
-#@Reuven
+# @author Reuven
 
 cd /bin
 COUNT=0
